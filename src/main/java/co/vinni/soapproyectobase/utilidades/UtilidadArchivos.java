e98ef6f67f9dc74021a256bb86b4fd907a08dfac
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2024-02-14 16:45:30 -0500

    funcionalidad: Adiciona funcionalidad de guardado de objetos en archivo y sus correspondientes test

--- src/main/java/co/vinni/soapproyectobase/utilidades/UtilidadArchivos.java
+++ src/main/java/co/vinni/soapproyectobase/utilidades/UtilidadArchivos.java
@@ -1,10 +1,28 @@
 package co.vinni.soapproyectobase.utilidades;
 
+import java.io.*;
+
 public class UtilidadArchivos {
-    public static void guardar(String archivo, Object objeto){
+    public static boolean guardar(String archivo, Object objeto){
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo));
+            oos.writeObject(objeto);
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
 
     }
     public static Object obtener(String archivo){
-        return null;
+        Object objetoCOnsultado;
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo));
+            objetoCOnsultado = ois.readObject();
+            return objetoCOnsultado;
+        } catch (IOException e) {
+            return null;
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
     }
 }
---
+++ src/test/java/co/vinni/soapproyectobase/utilidades/UtilidadArchivosTest.java
@@ -0,0 +1,30 @@
+package co.vinni.soapproyectobase.utilidades;
+
+import co.vinni.soapproyectobase.entidades.Equipo;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilidadArchivosTest {
+
+
+    @Test
+    @DisplayName("Test registrar objeto en archivo")
+    void registrarObjeto() {
+        Equipo equipo = new Equipo(1,"millos","Equipo de la capital Bogotá");
+        boolean rta = UtilidadArchivos.guardar("prueba.txt", equipo);
+        Assertions.assertTrue(rta);
+    }
+    @Test
+    @DisplayName("Test registrar objeto en archivo")
+    void leerObjeto() {
+        Equipo equipo = new Equipo(1,"millos","Equipo de la capital Bogotá");
+        UtilidadArchivos.guardar("prueba.txt", equipo);
+        Equipo elequipo = (Equipo)UtilidadArchivos.obtener ("prueba.txt");
+        System.out.println(elequipo);
+        Assertions.assertNotNull(elequipo);
+    }
+
+}
