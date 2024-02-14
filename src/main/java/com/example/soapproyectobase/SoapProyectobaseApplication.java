5fd4caee55836d50b503d39f80ce40323a4b12a5
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2024-02-14 13:57:17 -0500

    Vinni: Inicialización de repositorio

---
+++ src/main/java/com/example/soapproyectobase/SoapProyectobaseApplication.java
@@ -0,0 +1,17 @@
+package com.example.soapproyectobase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author Vinni 2023
+ */
+@SpringBootApplication
+public class SoapProyectobaseApplication {
+
+    public static void main(String[] args) {
+        //SpringApplication.run(SoapProyectobaseApplication.class, args);
+        System.out.println("proyecto base");
+    }
+
+}
---
+++ src/test/java/com/example/soapproyectobase/SoapProyectobaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soapproyectobase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapProyectobaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
