1b9c16fa91466a8c8d0eb203cdc4f2102aaa00e4
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2024-02-22 16:06:03 -0500

    Vinni: Actualiza la configuracion para implementar HTML

--- src/main/java/co/vinni/soapproyectobase/controladores/ControladorEquipos.java
+++ src/main/java/co/vinni/soapproyectobase/controladores/ControladorEquipos.java
@@ -7,22 +7,37 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 @Log4j2
+@Controller
+@RequestMapping("/equipos")
 public class ControladorEquipos {
     private static final Logger logger = LogManager.getLogger(ControladorEquipos.class);
 
     @Autowired
     ServicioEquipos servicioEquipos;
 
+    @RequestMapping("/guardar")
     public void registrarEquipo(EquipoDto equipo) {
         servicioEquipos.registrarEquipo(equipo);
     }
 
-    public List<EquipoDto> obtenerEquipos(){
+    @GetMapping({ "/"})
+    public String obtenerEquipos(Model model){
         logger.info("Verificando ");
-        return servicioEquipos.obtenerEquipos();
+        model.addAttribute("equipos",servicioEquipos.obtenerEquipos());
+        return "equipos";
+    }
+    @GetMapping
+    public String getEmpleado() {
+        return "empleado";
     }
 }
