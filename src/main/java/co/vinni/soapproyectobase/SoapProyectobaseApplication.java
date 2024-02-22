3eaf07700bc62e55878c6fda7c47eac019797df9
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2024-02-22 16:17:47 -0500

    Vinni: Actualiza el archivo de log y realiza la prueba del repositorio.

--- src/main/java/co/vinni/soapproyectobase/SoapProyectobaseApplication.java
+++ src/main/java/co/vinni/soapproyectobase/SoapProyectobaseApplication.java
@@ -1,8 +1,11 @@
 package co.vinni.soapproyectobase;
 
-import co.vinni.soapproyectobase.controladores.ControladorEquipos;
-import co.vinni.soapproyectobase.dto.EquipoDto;
+
+import co.vinni.soapproyectobase.entidades.Equipo;
+import co.vinni.soapproyectobase.repositorios.RespositorioEquipo;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,12 +16,30 @@ import java.util.List;
  */
 @SpringBootApplication
 @Log4j2
-public class SoapProyectobaseApplication {
+public class SoapProyectobaseApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SoapProyectobaseApplication.class, args);
         System.out.println("proyecto base");
 
     }
+    @Autowired
+    RespositorioEquipo repoEquipo;
+    @Override
+    public void run(String... args) throws Exception {
 
+
+        Equipo e = new Equipo();
+        e.setNombre("Millos");
+        e.setDescripcion("Ël equipo de bogota");
+
+        repoEquipo.save(e);
+
+       List<Equipo> listEquip = repoEquipo.findAll();
+       for (Equipo equipo : listEquip){
+           System.out.println(equipo);
+
+       }
+
+    }
 }
